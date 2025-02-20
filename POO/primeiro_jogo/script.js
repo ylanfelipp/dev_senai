daa2b1ddd5791256e94e12ff82485c13814dfcad
Ylan <ylanfelipp2@gmail.com>
2025-02-20 16:34:29 -0300

    Finalizando joguinho

--- POO/primeiro_jogo/script.js
+++ POO/primeiro_jogo/script.js
@@ -1,18 +1,30 @@
 const canvas = document.getElementById("jogoCanvas")
 const ctx = canvas.getContext('2d')
-
-document.addEventListener('click', e => {
-    if(gameOver) location.reload()
-})
-
 let gameOver = false
+let gravidade = 0.6
 
 const personagem = {
     posicaoX: 50,
     posicaoY: canvas.height - 50,
     largura: 50,
-    altura: 50
+    altura: 50,
+    velocidadeY: 0,
+    velocidadeX: 0,
+    pulando: false
 }
+
+document.addEventListener('click', e => {
+    if(gameOver) location.reload()
+})
+
+document.addEventListener('keypress', e => {
+    if (gameOver) return
+    if (e.code == "Space") {
+        personagem.velocidadeY = -15
+        personagem.pulando = true
+    }
+})
+
 
 function desenhaPersonagem() {
     ctx.fillStyle = "white"
@@ -44,12 +56,22 @@ function desenhaObstaculo() {
 
 function atualizaObstaculo() {
     obstaculo.posicaoX -= obstaculo.velocidade
+
+    if (obstaculo.posicaoX <= 0 - obstaculo.largura) {
+        let alturaObstaculo = Math.floor(Math.random() * 90) + 70
+        obstaculo.posicaoX = canvas.width - 100
+        obstaculo.altura = alturaObstaculo
+        obstaculo.posicaoY = canvas.height - alturaObstaculo
+        obstaculo.velocidade += 0.4
+    }
 }
 
 function verificaColisao() {
     if (
         personagem.posicaoX < obstaculo.posicaoX + obstaculo.largura &&
-        personagem.posicaoX + personagem.largura > obstaculo.posicaoX
+        personagem.posicaoX + personagem.largura > obstaculo.posicaoX &&
+        personagem.posicaoY < obstaculo.posicaoY + obstaculo.altura &&
+        personagem.posicaoY + personagem.altura > obstaculo.posicaoY
     ) {
         houveColisao()
     }
@@ -66,12 +88,32 @@ function houveColisao() {
     gameOver = true
 }
 
+function atualizaPersonagem() {
+    if (personagem.pulando) {
+        personagem.velocidadeY += gravidade
+        personagem.posicaoY += personagem.velocidadeY
+
+        if (personagem.posicaoY - personagem.velocidadeY > 350) {
+            personagem.posicaoY = 350
+            personagem.pulando = false
+        }
+    }
+
+    if (gameOver) {
+        personagem.velocidadeX = 0
+        return
+    }
+    personagem.velocidadeX = 0.2
+    personagem.posicaoX += personagem.velocidadeX
+} 
+
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     verificaColisao()
     desenhaObstaculo()
     desenhaPersonagem()
     atualizaObstaculo()
+    atualizaPersonagem()
     requestAnimationFrame(loop)
 }
 
